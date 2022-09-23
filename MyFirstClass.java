f998e72d24b4095e25c842d64830a614edc6d9b1
DjanaB <52209243@edu.campus02.at>
2022-09-23 17:03:41 +0200

    Commit initial

---
+++ MyFirstClass.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+
+   // wir haben unsere erste Klasse erzeugt
+
+    //psvm
+
+    public static void main(String[] args) {
+       // erste Ausgabe
+
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        // args
+        // sout + TAB
+
+        System.out.println(Arrays.toString(args));
+    }
+}
